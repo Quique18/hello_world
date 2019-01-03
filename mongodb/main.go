9e7707084058b7fe5fd5ea7f1d31d8d63e6852fa
Quique18 <45481499+Quique18@users.noreply.github.com>
2019-01-03 11:27:46 +0100

    Add files via upload

--- mongodb/main.go
+++ mongodb/main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-	"fmt"
     "encoding/json"
     "github.com/gorilla/mux"
     "log"
     "net/http"
 	"gopkg.in/mgo.v2"
-
+        "gopkg.in/mgo.v2/bson"
 )
 
 // The person Type (more like an object)
 type Person struct {
-    ID        string
-    Firstname string
-    Lastname  string
-    Address   *Address
+    ID        string   `json:"id,omitempty"`
+    Firstname string   `json:"firstname,omitempty"`
+    Lastname  string   `json:"lastname,omitempty"`
+    Address   *Address `json:"address,omitempty"`
 }
 type Address struct {
-    City  string
-    State string
+    City  string `json:"city,omitempty"`
+    State string `json:"state,omitempty"`
 }
 
 var people []Person
 var result []Person
+var resultId []Person
+
+var session, err = mgo.Dial("localhost")
 
 func Welcome(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Welcome!\n"))
@@ -31,75 +33,70 @@ func Welcome(w http.ResponseWriter, r *http.Request) {
 
 // Display all from the people var
 func GetPeople(w http.ResponseWriter, r *http.Request) {
-    w.Write((json.NewEncoder(w).Encode(result)))
-}
-
-// Display a single data
-func GetPerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for _, item := range people {
-        if item.ID == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            return
-        }
-    }
-    json.NewEncoder(w).Encode(&Person{})
-}
-
-// create a new item
-func CreatePerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    var person Person
-    _ = json.NewDecoder(r.Body).Decode(&person)
-    person.ID = params["id"]
-    people = append(people, person)
-    json.NewEncoder(w).Encode(people)
-}
-
-// Delete an item
-func DeletePerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for index, item := range people {
-        if item.ID == params["id"] {
-            people = append(people[:index], people[index+1:]...)
-            break
-        }
-        json.NewEncoder(w).Encode(people)
-    }
-}
-
-// main function to boot up everything
-func main() {
-
-	session, err := mgo.Dial("localhost")
-
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-
-	c := session.DB("PruebaDB").C("person")
-	err = c.Insert(&Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}},
-	&Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	if err != nil {
-			log.Fatal(err)
-	}
-
-
-		
-	err = session.DB("PruebaDB").C("person").Find(nil).All(&result)
+err = session.DB("PruebaDB").C("person").Find(nil).All(&result)
     if err != nil {
     	log.Fatal(err)
 	}
 
-	fmt.Println("Person: ", result)
+    json.NewEncoder(w).Encode(result)
+}
 
+// Display a single data
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+vars := mux.Vars(r)
+id := vars["ID"]
+
+    err = session.DB("PruebaDB").C("person").Find( bson.M{"ID": id}).All(&resultId)
+    if err != nil {
+    	log.Fatal(err)
+	}
+
+    json.NewEncoder(w).Encode(resultId)
+}
+
+// create a new item
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+//vars := mux.Vars(r)
+//id := vars["ID"]
+w.Write([]byte("Persona Creada!\n"))
+	c := session.DB("PruebaDB").C("person")
+	err = c.Insert(&Person{ID: "6", Firstname: "Create", Lastname: "Create", Address: &Address{City: "City X", State: "State X"}})
+	 if err != nil {
+	 		log.Fatal(err)
+	 }
+
+
+}
+
+// Delete an item
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+vars := mux.Vars(r)
+idd := vars["id"]
+	err = session.DB("PruebaDB").C("person").Remove( bson.M{"ID": idd})
+
+if err != nil {
+	log.Fatal(err)
+}
+}
+
+
+// main function to boot up everything
+func main() {
+
+	//defer session.Close()
+
+	//c := session.DB("PruebaDB").C("person")
+	// err = c.Insert(&Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}},
+	// &Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	// if err != nil {
+	// 		log.Fatal(err)
+	//}
 
     router := mux.NewRouter()
     router.HandleFunc("/", Welcome).Methods("GET")
     router.HandleFunc("/people", GetPeople).Methods("GET")
     router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-    router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-    router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":8001", router))
+    router.HandleFunc("/people/add/{id}", CreatePerson).Methods("POST")
+    router.HandleFunc("/people/del/{id}", DeletePerson).Methods("DELETE")
+    http.ListenAndServe(":8001", router)
 }
